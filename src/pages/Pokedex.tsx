f7445a5fc0f3d660f3fc93159f02db230104a328
Alexandre Junior <alexandrejunior408@gmail.com>
2022-06-28 16:48:38 -0300

    criação do campo de pesquisa da página pokedex

--- src/pages/Pokedex.tsx
+++ src/pages/Pokedex.tsx
@@ -1,3 +1,24 @@
+import { Header } from 'src/components/Header';
+import IconSearch from '../assets/imgs/IconSearch.svg';
+import '../styles/Pokedex.scss';
 export const Pokedex = () => {
-  return <div>Pokedex</div>;
+  return (
+    <>
+      <Header />
+      <section>
+        <div className="bgSearchPoke container">
+          <h1>Mais de 250 Pokemons para você escolher o seu favorito</h1>
+          <div className="bgInputSearch">
+            <input
+              type="search"
+              id="input"
+              placeholder="Pesquisar pokemon"
+              maxLength={20}
+            />
+            <img src={IconSearch} alt="" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
