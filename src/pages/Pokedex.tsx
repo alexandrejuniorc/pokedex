0e1a7d359ffcf862ca04b16d5e3ca848fca7ab80
Alexandre Junior <alexandrejunior408@gmail.com>
2022-06-29 21:16:30 -0300

    inicio de criação da lógica para a requisição do endpoint dos pokemons

--- src/pages/Pokedex.tsx
+++ src/pages/Pokedex.tsx
@@ -1,7 +1,56 @@
-import { Header } from 'src/components/Header';
-import IconSearch from '../assets/imgs/IconSearch.svg';
+// styles - scss
 import '../styles/Pokedex.scss';
+// react uses - useState, useEffect
+import { useEffect, useState } from 'react';
+// icon
+import IconSearch from '../assets/imgs/IconSearch.svg';
+// components
+import { Header } from 'src/components/Header';
+// axios
+import axios from 'axios';
+
+//interface
+interface IPoke {
+  name: string;
+}
+
 export const Pokedex = () => {
+  const [pokemons, setPokemons] = useState([]);
+  const getAllPokemons = async () => {
+    const { data }: any = await axios
+      .get('https://pokeapi.co/api/v2/pokemon?offset=5&limit=5')
+      .catch((error) => console.log(error));
+    const results: any = await data.results;
+    // returns a pokemon object
+    // console.log(results);
+
+    const newPokes: any = pokemons;
+    // return pokemon skills
+    // console.log(newPokes);
+
+    for (const pokemon of results) {
+      const { data } = await axios.get(pokemon.url);
+      // return pokemon skills
+      // console.log(data);
+
+      const newPoke = Object.assign({ ...pokemon }, data);
+      // returns each pokemon's object along with its abilities
+      // console.log(newPoke);
+
+      newPokes.push(newPoke);
+      // returns an object with all pokemons
+      // console.log(newPokes);
+    }
+    setPokemons(newPokes);
+  };
+
+  useEffect(() => {
+    getAllPokemons();
+  }, []);
+
+  console.log(pokemons);
+  /*  console.log(pokemons[0].sprites.other.dream_world.front_default); */
+
   return (
     <>
       <Header />
@@ -17,6 +66,17 @@ export const Pokedex = () => {
             />
             <img src={IconSearch} alt="" />
           </div>
+        </div>
+      </section>
+      <section>
+        <div></div>
+        <div>
+          {pokemons &&
+            pokemons?.map(({ name }: IPoke) => (
+              <div key={name}>
+                <p>{name}</p>
+              </div>
+            ))}
         </div>
       </section>
     </>
